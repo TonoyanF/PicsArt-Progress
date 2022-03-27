126911917581316f27d63ce4ad0b2112e17030a6
TonoyanF <101404073+TonoyanF@users.noreply.github.com>
2022-03-27 15:11:33 +0400

    Update Homework_1.12.py

--- Homework_1.12.py
+++ Homework_1.12.py
@@ -1,5 +1,4 @@
 def pascal(n,m):
-    if 1 in (n,m) or n == m:
+    if m == 1 or n == m:
         return 1
-    else:
-        return pascal(n-1,m-1) + pascal(n,m-1)    
+    return pascal(n-1,m-1) + pascal(n,m-1)    
